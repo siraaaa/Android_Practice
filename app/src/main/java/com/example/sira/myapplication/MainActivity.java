bc4cddea6dfdddd62f8b9a808ca4b6aafde4cd4d
siraaaa <30367776+siraaaa@users.noreply.github.com>
2018-09-10 22:53:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sira/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sira.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btn1:
+                Toast.makeText(this, "SIRA", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
